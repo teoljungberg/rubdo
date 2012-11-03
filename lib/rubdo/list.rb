7081f394834ae9bd61ba4405ef265be4d536901f
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-11-03 14:05:07 +0100

    Cleaned up some old code

--- lib/rubdo/list.rb
+++ lib/rubdo/list.rb
@@ -4,11 +4,13 @@ require 'fileutils'
 
 module Rubdo
   class List
-    attr_reader :items, :completed, :storage
+    attr_reader :items, :storage
 
     def self.read(file)
-      return YAML.load_file(file) if File.exists? file
-      []
+      todos = YAML.load_file(file) if File.exists? file
+      todos ||=[]
+
+      todos
     end
 
     def initialize(todos)

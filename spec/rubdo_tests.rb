863304078de3b8b47f9adf38bdd7c75d5528f23a
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-11-08 11:50:40 +0100

    Implemented rspec tests instead

--- spec/rubdo_tests.rb
+++ spec/rubdo_tests.rb
@@ -1,30 +1,34 @@
-require 'minitest/autorun'
+require 'rspec'
 require 'rubdo'
 
 describe Rubdo::Item do
   before do
-    @item = Rubdo::Item.new('New todo item')
+      @item = Rubdo::Item.new('New todo item')
   end
 
-  it 'assigns a text value to rubdo' do
-    @item.description.must_equal 'New todo item'
+  describe '#new' do
+    it 'assigns a text value to rubdo' do
+      @item.description.should eq('New todo item')
+    end
   end
 end
 
 describe Rubdo::List do
-  before do
-    @list = Rubdo::List.new(Rubdo::List.read(File.join(File.dirname(__FILE__), 'test.yml')))
-    @list.add('Buy milk')
-    @list.add('Buy beer')
-    @list.add('But peanuts')
-  end
+  describe '#add' do
+    before do
+      @list = Rubdo::List.new(Rubdo::List.read(File.join(File.dirname(__FILE__), 'test.yml')))
+      @list.add('Buy milk')
+      @list.add('Buy beer')
+      @list.add('But peanuts')
+    end
 
-  it 'checks if the all added items are there' do
-    @list.to_a.size.must_equal 3
-  end
+    it 'checks if the all added items are there' do
+      @list.to_a.size.should equal 3
+    end
 
-  it 'deletes a todo item' do
-    @list.done(0).description.must_equal 'Buy milk'
-    @list.to_a.size.must_equal 2
+    it 'deletes a todo item' do
+      @list.done(0).description.should eq('Buy milk')
+      @list.to_a.size.should equal 2
+    end
   end
 end

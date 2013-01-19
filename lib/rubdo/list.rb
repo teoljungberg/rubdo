7e5b47aff0e886d12aaeba6bb3ab41137a78f237
Teo Ljungberg <teo.ljungberg@gmail.com>
2013-01-19 16:48:51 +0100

    Added spec for list and refactored the class

--- lib/rubdo/list.rb
+++ lib/rubdo/list.rb
@@ -7,37 +7,36 @@ module Rubdo
     attr_reader :items, :todo_file
 
     def self.read(file = nil)
-      todo_file = File.expand_path file
+      todo_file = File.expand_path(file)
       items = File.exists?(todo_file) ? YAML.load_file(todo_file) : []
       { items: items, todo_file: todo_file }
     end
 
     def initialize(options)
-      @items = options[:items]
-      @todo_file = options[:todo_file]
+      @items = options.fetch(:items) { [] }
+      @todo_file = options.fetch(:todo_file) { File.expand_path("~/.tasks/Todo.yml") }
     end
 
     def add(description)
-      @items << Item.new(description)
+      items << Item.new(description)
     end
     alias_method :<<, :add
 
     def done(id)
-      @items.delete_at id
+      items.delete_at id
     end
 
     def edit(id)
-      @items[id]
+      items[id]
     end
 
     def write
-      File.open(@todo_file, 'w') { |f| f.write(@items.to_yaml) }
+      File.open(todo_file, 'w') { |f| f.write(items.to_yaml) }
     end
     alias_method :save, :write
 
     def to_a
-      @items
+      items
     end
-
   end
 end
